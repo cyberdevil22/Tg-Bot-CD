892f0437aa6d5723610f430a7332a45479ccc751
cyberdevil22 <125547987+cyberdevil22@users.noreply.github.com>
2023-02-17 00:14:31 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 token = "6081113959:AAGFPT4zR9kA1PtllK7WEErFcr9vheuFjuk"
 admin_id = 756439645
 
-file_1 = open("edit_qiwi_money_number.txt", "r")
-qiwi_number = file_1.read()
+# file_1 = open("edit_qiwi_money_number.txt", "r")
+# qiwi_number = file_1.read()
 
-file_2 = open("edit_qiwi_money_token.txt", "r")
-token_qiwi = file_2.read()
+# file_2 = open("edit_qiwi_money_token.txt", "r")
+# token_qiwi = file_2.read()
